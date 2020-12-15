c17cf1b498a2e339334aae8fef7a54bd826d7925
YevheniiSych <evgeniysych18@gmail.com>
2020-12-15 21:42:40 +0200

    Bug fixes

--- app/src/main/java/com/maryna/photographer/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/maryna/photographer/ui/fragments/HomeFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.maryna.photographer.MainActivity;
 import com.maryna.photographer.R;
 
 public class HomeFragment extends Fragment {
@@ -17,5 +20,12 @@ public class HomeFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        TextView orderBtn = view.findViewById(R.id.orderPhotoSessionBtn);
+        orderBtn.setOnClickListener(v -> ((MainActivity) getActivity()).getNavController().navigate(R.id.navigation_portfolio));
+        super.onViewCreated(view, savedInstanceState);
     }
 }
--- app/src/main/java/com/maryna/photographer/ui/fragments/OrderFragment.java
+++ app/src/main/java/com/maryna/photographer/ui/fragments/OrderFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.maryna.photographer.MainActivity;
 import com.maryna.photographer.R;
 
 public class OrderFragment extends Fragment {
@@ -90,5 +91,6 @@ public class OrderFragment extends Fragment {
         emailIntent.putExtra(Intent.EXTRA_EMAIL, "evgeniysych18@gmail.com"); // email фотографа
         emailIntent.putExtra(Intent.EXTRA_TEXT, emailBody);
         startActivity(Intent.createChooser(emailIntent, "Send email"));
+        ((MainActivity)getActivity()).getNavController().navigate(R.id.action_orderFragment_to_homeFragment);
     }
 }
